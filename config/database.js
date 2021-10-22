19c0a76129a3d54d5f62eab36c67d6b073e7d411
Gabriel Azevedo <azevgabriel@gmail.com>
2021-10-22 17:26:12 -0300

    feat: update connection db

--- config/database.js
+++ config/database.js
@@ -71,13 +71,14 @@ module.exports = {
   |
   */
   pg: {
-    client: 'pg',
+    client: 'postgres',
     connection: {
       host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      database: Env.get('DB_DATABASE', 'adonis'),
+      ssl: { rejectUnauthorized: false }
     },
     debug: Env.get('DB_DEBUG', false)
   }
